9b0bf4f5b19fcb438b25eb560abbdb381ce64478
Natalie Finnegan <nfinneg2@gmail.com>
2021-06-26 12:45:26 -0600

    firebase, bootstrap, dotenv install, sign up comp

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Signup from "../../komponent/Signup";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Welcome to Dollar Sense</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Welcome to Dollar Sense</h1>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
 
-export default Home
+        <Signup />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
