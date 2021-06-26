16a7456792a8ab47315af54bb38b324bfc49dd87
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-06-26 10:39:46 -0500

    add routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     //Set up real routes
+    <Router>
+      {/* <Preloader load={load} /> */}
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        {/* <ScrollToTop /> */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/about" component={About} />
+          <Route path="/budget" component={Budget} />
+          <Route path="/invest" component={Invest} />
+          <Route path="/goals" component={Goals} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
     
     // <div className="App">
     //   <div className="App-header">
