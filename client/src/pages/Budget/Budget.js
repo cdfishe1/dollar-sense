7e144b3eb1cfead45e2b708a7311e8dca5b2e3b2
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-07-07 18:59:14 -0500

    reintroduce total and table for budget

--- client/src/pages/Budget/Budget.js
+++ client/src/pages/Budget/Budget.js
@@ -1,11 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./budget.css";
 //import ScriptTag from "react-script-tag";
 import { auth } from "../../Firebase";
 
+
+
 const Budget = () => {
   const [tName, setTName] = useState("");
   const [amount, setAmount] = useState(0.0);
+  // let transactions = useRef(null);
+  let transactions = useRef();
+  
+
+  useEffect(() => {
+    
+    fetch("/api/transaction")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      // save db data on global variable
+      transactions = data;
+
+      populateTotal();
+      populateTable();
+      // populateChart();
+
+      
+}, []);
+
+  function populateTotal() {
+    // reduce transaction amounts to a single total value
+    let total = transactions.reduce((total, t) => {
+      return total + parseInt(t.value);
+    }, 0);
+
+    let totalEl = document.querySelector("#total");
+    totalEl.textContent = total;
+  }
+
+  function populateTable() {
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+
+    transactions.forEach(transaction => {
+      // create and populate a table row
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+
+      tbody.appendChild(tr);
+    });
+  }
+
+  function populateTable() {
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+
+    transactions.forEach(transaction => {
+      // create and populate a table row
+      let tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${transaction.name}</td>
+        <td>${transaction.value}</td>
+      `;
+
+      tbody.appendChild(tr);
+    });
+  }
+  });
+
+  
 
   const addBtn = () => {
     let transaction = {
